831bfb3ccee6134edf09a35a1e0c49eedbff0e01
Prixgen Developments <50573664+satishprixgen@users.noreply.github.com>
2020-08-14 12:12:54 +0530

    pack_slip

--- pack_slip/__manifest__.py
+++ pack_slip/__manifest__.py
@@ -1,13 +1,12 @@
 {
     'name': 'Venturific Pack Print',
-    'version': '13.0.0.6',
+    'version': '13.0.0.7',
     'category': 'Inventory',
     'author': 'Prixgen Tech Solutions Pvt Ltd.',
     'company': 'Prixgen Tech Solutions Pvt Ltd.',
     'website': 'https://www.prixgen.com',
     'depends': ['base','stock','sale','account'],
     'data': [
-        'reports/single_pack.xml',
         'data/sequence.xml',
         'wizards/print_doc.xml',
         'views/sale_order.xml',
@@ -15,6 +14,7 @@
         'views/stock_quants_package.xml',
         'reports/pack_report_inherit.xml',
         'reports/sale_order_pack_print.xml',
+        'reports/single_pack.xml',
 
         
     ],
